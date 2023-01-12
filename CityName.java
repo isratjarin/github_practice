4ffa84777eb4e9fbdec7f9c975c81202a8b72182
rana <isratjarin115@gmail.com>
2023-01-11 22:19:27 -0600

    Initial Commit

---
+++ CityName.java
@@ -0,0 +1,17 @@
+public class CityName {
+    public static void main(String[] args) {
+        String city= "Dhaka";
+        if (city=="USA") {
+            System.out.println("USA");
+        }
+        else if(city== "Canada") {
+            System.out.println("Canada");
+        }
+        else if(city== "Barisal") {
+            System.out.println("Barisal");
+        }
+        else {
+            System.out.println(city);
+        }
+    }
+}
